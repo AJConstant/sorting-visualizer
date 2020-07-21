5f0f4c679864db89409cc56b51fe4e1f10f0c199
Alex Constant <alexanderjconstant@gmail.com>
2020-07-20 21:34:27 -0500

    Updates

---
+++ precache-manifest.0f049f44e2e17aace945448082b98620.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c859b5def932148bda45986dcea34c7e",
+    "url": "/sorting-visualizer/index.html"
+  },
+  {
+    "revision": "e91b4cf84ceb5c0c6b62",
+    "url": "/sorting-visualizer/static/js/2.90977576.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/sorting-visualizer/static/js/2.90977576.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86d2efc4a8a345e8292d",
+    "url": "/sorting-visualizer/static/js/main.f64fab38.chunk.js"
+  },
+  {
+    "revision": "0b849e312d4bb7a17285",
+    "url": "/sorting-visualizer/static/js/runtime-main.648a2344.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sorting-visualizer/precache-manifest.725f6c7c1f8b1b9d5a123486d9968189.js"
+  "/sorting-visualizer/precache-manifest.0f049f44e2e17aace945448082b98620.js"
 );
 
 self.addEventListener('message', (event) => {
